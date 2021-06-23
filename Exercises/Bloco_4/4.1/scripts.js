058e1e5aa1d058d6772a06e72f17582f9c30b13a
Vini_Vergani <vinivergani95@gmail.com>
2021-06-23 16:35:08 -0300

    Adicona arquivo aula 4.1

--- Exercises/Bloco_4/4.1/scripts.js
+++ Exercises/Bloco_4/4.1/scripts.js
@@ -1,13 +1,1 @@
-let age = 20;
-let name = "Hulk";
-let fullName = "Vine Vergani";
-birthCity = "Caxias do Sul";
-let birthYear = 1995;
-birthYear = 2030;
-birthCity = "Xablau"
-
-console.log(age);
-console.log(name);
-console.log(fullName);
-console.log(birthCity);
-console.log(birthYear);
+console.log('Santos meu ammmmooorrrr!');
