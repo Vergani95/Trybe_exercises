83fb14dd44f9ed62040022739a30d50618606051
Vini_Vergani <vinivergani95@gmail.com>
2021-06-27 15:53:39 -0300

    Add aula 4.1

---
+++ Exercises/Bloco_4_IntroJavaScript/4.1/Exers_posAula/Exer1.js
@@ -0,0 +1,9 @@
+let a = 8;
+let b = 2;
+
+if (a>b) {
+    console.log(a);
+}
+else {
+    console.log 
+}
---
+++ Exercises/Bloco_4_IntroJavaScript/4.1/Exers_posAula/Exer3.js
@@ -0,0 +1,20 @@
+let a = 8;
+let b = 12;
+let c = 4;
+let maior;
+let maiorn;
+
+if (a>b) {
+    maior= a;
+}
+else if (b>a) {
+    maior = b;
+}
+
+if (maior>c) {
+    maiorn = maior; 
+}
+else {
+    maiorn = c
+}
+console.log(maiorn)
