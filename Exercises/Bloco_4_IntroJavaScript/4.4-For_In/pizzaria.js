cdb22ca90bade454b892fd551cdc0662d15a764d
Vini_Vergani <vinivergani95@gmail.com>
2021-06-28 16:06:12 -0300

    Adiciona exemplo couse

---
+++ Exercises/Bloco_4_IntroJavaScript/4.4-For_In/pizzaria.js
@@ -0,0 +1,15 @@
+let pizzas = {
+    sabor: "palmito",
+    preco: 39.90 ,
+    bordaCatupiry: true
+}
+
+for (let key in pizzas) {
+    console.log (key);
+}
+
+let pizzasDoces =['Chocolate', 'Morango', 'sorvete'];
+
+for (let key in pizzasDoces) {
+    console.log (pizzasDoces[key]);
+}
