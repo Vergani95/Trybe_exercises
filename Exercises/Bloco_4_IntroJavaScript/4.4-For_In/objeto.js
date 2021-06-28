fda0a77cc47db229551a2194aa8b21dab243dafa
Vini_Vergani <vinivergani95@gmail.com>
2021-06-28 19:41:05 -0300

    add arquivos desenvolvidos na aula ao vivo

--- Exercises/Bloco_4_IntroJavaScript/4.4-For_In/objeto.js
+++ Exercises/Bloco_4_IntroJavaScript/4.4-For_In/objeto.js
@@ -9,6 +9,14 @@ console.log("meu nome é ", pessoaEstudane['nome'], pessoaEstudane['sobreNome'])
 
 pessoaEstudane.turma='Turma 14A';
 
-console.log(pessoaEstudane);
-delete pessoaEstudane.apelido;
-console.log(pessoaEstudane);
+// console.log(pessoaEstudane);
+// delete pessoaEstudane.apelido;
+// console.log(pessoaEstudane);
+
+for (let chaves in pessoaEstudane ) {
+    console.log(chaves);
+}
+
+for (let chaves in pessoaEstudane ) {
+    console.log(pessoaEstudane[chaves]);
+}
---
+++ Exercises/Bloco_4_IntroJavaScript/funcoes.js
@@ -0,0 +1,14 @@
+function imprimeResultado (textoParaImprimir) {
+    console.log(textoParaImprimir);
+}
+
+imprimeResultado('Trabalhar com função é muito mara!!');
+imprimeResultado('Como diria o Gus, isso é muito legal!!');
+
+function maiorNumero(num1, num2){
+    return Math.max(num1,num2);
+}
+console.log("o maior número é: ", maiorNumero(5,9));
+
+
+
